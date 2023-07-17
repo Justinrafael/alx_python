735cca6a745b7ae513f8de84d7b34d80816ff8e0
Justinrafael <udanohjustin@gmail.com>
2023-07-17 19:36:00 +0100

    fix for task 0

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,6 +1,22 @@
 Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:48:03) [MSC v.1928 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
+>>> Write a Python script that prints exactly "Programming is like building a multilingual puzzle
+SyntaxError: invalid syntax
+>>> print(" "Programming is like building a multilingual puzzle\n")
+      
+SyntaxError: invalid syntax
+>>> print("Programming is like building a multilingual puzzle\n)
+      
+SyntaxError: EOL while scanning string literal
 >>> print("Programming is like building a multilingual puzzle\n")
 Programming is like building a multilingual puzzle
+
+>>> print(' "Programming is like building a multilingual puzzle\n')
+ "Programming is like building a multilingual puzzle
+
+>>> 
+================================ RESTART: Shell ================================
+>>> print(' "Programming is like building a multilingual puzzle\n')
+ "Programming is like building a multilingual puzzle
 
 >>> 
