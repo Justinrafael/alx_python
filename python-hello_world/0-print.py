5ac2bdc3427e78f01b0831134f56025ed26801dd
Justinrafael <udanohjustin@gmail.com>
2023-07-17 18:15:06 +0100

    task 0

--- python-hello_world/0-print.py
+++ python-hello_world/0-print.py
@@ -1,1 +1,6 @@
-print("Programming is like building a multilingual puzzle\n")
+Python 3.8.10 (tags/v3.8.10:3d8993a, May  3 2021, 11:48:03) [MSC v.1928 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print("Programming is like building a multilingual puzzle\n")
+Programming is like building a multilingual puzzle
+
+>>> 
