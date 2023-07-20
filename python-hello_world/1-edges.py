86aaff8cde6cc79627a81fb6eb6e4483dae1b572
Justinrafael <udanohjustin@gmail.com>
2023-07-20 06:36:40 -0700

    task 1

--- python-hello_world/1-edges.py
+++ python-hello_world/1-edges.py
@@ -1,8 +1,7 @@
-#!/usr/bin/python3
 word = "Holberton"
 word_first_3 = word[:3]
 word_last_2 = word[-2:]
 middle_word = word[1:-1]
-print("first 3 letters: {}".format(word_first_3),end = "\n")
-print("last 2 letters: {}".format(word_last_2),end = "\n")
-print("middle letter: {}".format(middle_word),end = "\n")
+print("first 3 letters: {}".format(word_first_3),end="\n")
+print("last 2 letters: {}".format(word_last_2),end="\n")
+print("middle letter: {}".format(middle_word),end="\n")
