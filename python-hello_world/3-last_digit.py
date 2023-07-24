b07e9ddd18c14595bee530b8e69116ad3affd0b3
Justinrafael <udanohjustin@gmail.com>
2023-07-24 18:16:31 +0100

    task 3

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -8,8 +8,8 @@ else:
     print("Last digit of", number, "is", last_digit, end="")
 
 if last_digit > 5 and number > 0:
-    print(" and is greater than 5")
+    print(" and is greater than 5",end="\n")
 elif last_digit == 0:
-    print(" and is 0")
+    print(" and is 0", end="\n")
 else:
-    print(" and is less than 6 and not 0")
+    print(" and is less than 6 and not 0", end="\n")
