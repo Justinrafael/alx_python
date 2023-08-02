8d23e79c844f60e884007bacb398acb4e2474680
Justinrafael <udanohjustin@gmail.com>
2023-08-02 19:08:16 +0100

    task 0

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,4 +1,7 @@
-from add_0 import add
-a = 1
-b = 2
-print(a + b)
+if __name__ == "__main__":
+    from add_0 import add
+
+    a = 1
+    b = 2
+
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
