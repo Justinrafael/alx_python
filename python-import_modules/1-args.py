14da8a9e94e48162ede9607fb2b0ef569836eb79
Justinrafael <udanohjustin@gmail.com>
2023-08-02 20:21:29 +0100

    task 3

--- python-import_modules/1-args.py
+++ python-import_modules/1-args.py
@@ -1,5 +1,19 @@
+#!/usr/bin/env python3
+"""
+This module prints the number of and the list of its arguments.
+"""
+
 if __name__ == "__main__":
-    from add_0 import argv
-    
-    cars = ["volvo", "benz", "toyota"]
-    print(cars)
+    from sys import argv
+
+    argc = len(argv) - 1
+
+    if argc == 0:
+        print("{:d} argument.".format(argc))
+    elif argc == 1:
+        print("{:d} argument:".format(argc))
+    else:
+        print("{:d} arguments:".format(argc))
+
+    for i in range(1, argc + 1):
+        print("{:d}: {}".format(i, argv[i]))
